c207d1629017655d90e6ed60f016cb32e82e7c3f
Michael Aichmueller <m.aichmueller@gmail.com>
2024-01-03 15:29:47 +0100

    rm templates and other obsolete packaging stuff

--- py/interface.hpp
+++
@@ -1,6 +0,0 @@
-#include "fancy/fancy.hpp"
-
-// template class fancy::Fancy++Gadget<int>;
-// template class fancy::Fancy++Gadget<unsigned int>;
-// template class fancy::Fancy++Gadget<float>;
-// template class fancy::Fancy++Gadget<bool>;
---
+++ py/linkdef.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#pragma link C++ class TemplateVectorFormatter<int>;
+#pragma link C++ class TemplateVectorFormatter<std::string>;
